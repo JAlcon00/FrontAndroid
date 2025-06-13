72be890d8735df2edcb8fe54abc3dd2b0ccb3c6c
Hassiel Monjaraz <HassielAM_012@outlook.com>
2025-06-13 15:41:30 -0600

    card perfilUsuario

--- app/src/main/java/com/example/frontstore/presentation/screens/PerfilUsuarioScreen.kt
+++ app/src/main/java/com/example/frontstore/presentation/screens/PerfilUsuarioScreen.kt
@@ -1,2 +1,45 @@
-package com.example.frontstore.presentation.screens
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Composable
+fun PerfilOpcionCard(titulo: String, subtitulo: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFCDC6D9))
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = titulo, style = MaterialTheme.typography.titleSmall)
+                Text(text = subtitulo, color = Color.Gray, fontSize = 12.sp)
+            }
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = "Ir",
+                tint = Color.Gray
+            )
+        }
+    }
+}
