1db91b16e0cf48412fd24ba727a0045c1e79177d
Hassiel Monjaraz <HassielAM_012@outlook.com>
2025-06-13 15:32:33 -0600

    card para ListaArticulos

--- app/src/main/java/com/example/frontstore/presentation/screens/ListaArticulosScreen.kt
+++ app/src/main/java/com/example/frontstore/presentation/screens/ListaArticulosScreen.kt
@@ -1,14 +1,19 @@
 package com.example.frontstore.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.frontstore.presentation.components.CardArticulo
 import com.example.frontstore.presentation.viewmodel.ArticuloViewModel
 
@@ -20,6 +25,50 @@ data class ArticuloUiState(
     val error: String? = null,
     val articulos: List<Articulo> = emptyList()
 )
+
+
+
+
+// --- Componente de tarjeta ---
+@Composable
+fun CardArticulo(
+    titulo: String,
+    imagenUrl: String,
+    precio: Double,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 4.dp),
+        onClick = onClick
+    ) {
+        Column(modifier = Modifier.padding(2.dp)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp) // Ajusta la altura según lo que necesites
+                    .padding(2.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(imagenUrl),
+                    contentDescription = titulo,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                Text(text = titulo, style = MaterialTheme.typography.titleMedium)
+                Text(text = "$${precio}", style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(vertical = 2.dp))
+            }
+        }
+    }
+
+}
 
 
 
