cfe8fdbb41f9686b4ea0d310f46f31bc7bdf43d7
Hassiel Monjaraz <HassielAM_012@outlook.com>
2025-06-13 15:42:44 -0600

    preview de perfilUsuario

--- app/src/main/java/com/example/frontstore/presentation/screens/PerfilUsuarioScreen.kt
+++ app/src/main/java/com/example/frontstore/presentation/screens/PerfilUsuarioScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -126,4 +127,13 @@ fun PerfilUsuarioScreen() {
             subtitulo = "Direcciones guardadas en tu cuenta"
         )
     }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PerfilUsuarioScreenPreview() {
+    PerfilUsuarioScreen()
 }
